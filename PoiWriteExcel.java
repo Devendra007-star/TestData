b67c303b6ef2fc0d05899576dfc1587042e6cfab
Devendra007-star <devendra.makode@otsi.ca.gov>
2024-10-11 20:54:35 +0530

    Write Excel file

--- PoiWriteExcel.java
+++ PoiWriteExcel.java
@@ -1,62 +1,102 @@
 package cares.cwds.salesforce.common.utilities;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.logging.Logger;
-
-import javax.swing.JOptionPane;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.util.Map.Entry;
 
 public class PoiWriteExcel {
-	
-	
-			private PoiWriteExcel() {
-				// this is empty
-			}
-			private static final Logger logger = Logger.getLogger(PoiReadExcel.class.getName());
-			
-			public static void main(String[] args) {
-				
-				try (
-						FileInputStream file = new FileInputStream(new File("src/main/java/Artifacts/TestData/SD/ScreeningData.xlsx"));
-						 XSSFWorkbook workbook = new XSSFWorkbook(file)) {
-						XSSFSheet sheet = workbook.getSheet("ScreeningData");
-						int rowNum = sheet.getLastRowNum()+1;
-						int cellNum = sheet.getRow(0).getLastCellNum();
-						
-						Map<String, Integer> colByName = new HashMap<>();
-						if(sheet.getRow(0).cellIterator().hasNext()) {
-							for (int i = 0; i < cellNum; i++) {
-								colByName.put(new DataFormatter().formatCellValue(sheet.getRow(0).getCell(i)).toString(), i);
-							}
-						}
-						Row row = sheet.createRow(rowNum);
-						for (int j = 0; j <cellNum; j++) {
-							if(colByName.get("SCREEENING_ID") == j) {
-								row.createCell(j).setCellValue("Test123");
-							}
-							else if(colByName.get("SC_TSP")== j) {
-								row.createCell(j).setCellValue(LocalDateTime.now().toString());
-							}
-						}
-						FileOutputStream output = new FileOutputStream(new File("src/main/java/Artifacts/TestData/SD/ScreeningData.xlsx"));
-						workbook.write(output);
-						output.close();
-						
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-}
+
+	    public static String filePath = "src/main/java/Artifacts/TestData/SD/ScreeningData.xlsx";
+	    public static String writeSheetName = "ScreeningData";
+
+	    public static void main(String[] args) throws IOException {
+	        String[] fields = {"SCREENING_ID", "SC_TSP"};
+	        String[] fieldNames = {"SCREENING_ID", "SC_TSP", "RETRIEVE_SCDATA", "RD_TSP"};
+	        String screeningId = "Deve98765";
+	        PoiWriteExcel ex = new PoiWriteExcel();
+
+	        ex.writeExcelData(filePath, writeSheetName, fields, screeningId);
+	        ex.getWriteScreeningId(filePath, writeSheetName, fieldNames, screeningId);
+	    }
+
+	    public void writeExcelData(String filePath, String sheetName, String[] fieldName, String fieldValue) {
+	        Map<String, Integer> colByName = new HashMap<>();
+
+	        try (FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+	             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream)) {
+	            Sheet sheet = workbook.getSheet(sheetName);
+
+	            for (int i = 0; i < sheet.getRow(0).getLastCellNum(); i++) {
+	                colByName.put(new DataFormatter().formatCellValue(sheet.getRow(0).getCell(i)), i);
+	            }
+	            Row newRow = sheet.createRow(sheet.getLastRowNum() + 1);
+
+	            for (Entry<String, Integer> entry : colByName.entrySet()) {
+	                if (fieldName[0].equals(entry.getKey())) {
+	                    newRow.createCell(entry.getValue()).setCellValue(fieldValue);
+	                } else if (fieldName[1].equals(entry.getKey())) {
+	                    newRow.createCell(entry.getValue()).setCellValue(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
+	                }
+	            }
+	            try (FileOutputStream outputStream = new FileOutputStream(new File(filePath))) {
+	                workbook.write(outputStream);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public void getWriteScreeningId(String filePath, String sheetName, String[] fieldNames, String screeningId) {
+	        Map<String, Integer> colByName = new HashMap<>();
+
+	        try (FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+	             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream)) {
+	            Sheet sheet = workbook.getSheet(sheetName);
+
+	            Row headerRow = sheet.getRow(0);
+
+	            for (int i = 0; i < headerRow.getLastCellNum(); i++) {
+	                String columnName = new DataFormatter().formatCellValue(headerRow.getCell(i));
+	                colByName.put(columnName, i);
+	            }
+
+	            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+	                Row currentRow = sheet.getRow(i);
+	                if (currentRow != null) {
+	                    String scDataValue = new DataFormatter()
+	                            .formatCellValue(currentRow.getCell(colByName.get(fieldNames[2])));
+
+	                    if (scDataValue.isEmpty()) {
+	                        String getWriteScreeningId = new DataFormatter().formatCellValue(currentRow.getCell(colByName.get(fieldNames[0])));
+	                       if (currentRow.getCell(colByName.get(fieldNames[2])) == null) {
+	                            currentRow.createCell(colByName.get(fieldNames[2])).setCellValue("USED");
+	                        } else {
+	                            currentRow.getCell(colByName.get(fieldNames[2])).setCellValue("USED");
+	                        }
+
+	                        if (currentRow.getCell(colByName.get(fieldNames[3])) == null) {
+	                            currentRow.createCell(colByName.get(fieldNames[3])).setCellValue(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
+	                        } else {
+	                            currentRow.getCell(colByName.get(fieldNames[3])).setCellValue(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yy HH:mm")));
+	                        }
+	                    }
+	                }
+	            }
+
+	            try (FileOutputStream outputStream = new FileOutputStream(new File(filePath))) {
+	                workbook.write(outputStream);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+
